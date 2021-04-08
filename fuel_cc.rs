1aa021360a534d5840ab9b1448bebff391ea48ff
Aaron N. Josserand-Austin <anjaustinftw@gmail.com>
2021-04-08 15:07:27 -0600

    Added fuel_cc.rs

---
+++ fuel_cc.rs
@@ -0,0 +1,8 @@
+// Fuel Consumption Calculator
+// INTERFACES
+//  INPUTS:    {start_miles, end_miles, refilled_gallons}
+//  OUTPUTS:   {miles_driven, refilled_gallons, miles_per_gallon}
+//
+// IMPLEMENTATION:
+//  Calculate: miles_driven/refilled_gallons
+
